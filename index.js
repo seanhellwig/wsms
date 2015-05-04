43c407461f78d67fea5904f2f9c22b0cc22f3473
Sean Hellwig <seanhellwig@gmail.com>
2015-05-04 19:22:04 -0400

    Env variable

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var cfenv = require('cfenv');
 var appEnv = cfenv.getAppEnv();
 var mongo = require('mongodb').MongoClient;
 var twilio = require('twilio');
-var twilio_client = new twilio.RestClient(appEnv.twilio_sid, appEnv.twilio_client);
+var twilio_client = new twilio.RestClient(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 var server;
 var mongoUrl;
 var services = appEnv.services;
 var db;
-   
+
 if (services['mongodb-2.4']) {
 	mongoUrl = services['mongodb-2.4'][0].credentials.url;
 }
@@ -25,18 +25,18 @@ app.post('/receive', function(req, res) {
 });
 
 
-// Test route
-app.get('/send', function(req, res) {
-  twilio_client.sms.messages.post({
-    to:'',
-    from:'',
-    body:'Hello World'
-  }, function(error, message) {
-      if (error) {
-          console.log(error.message);
-      }
-  });
-});
+// // Test route
+// app.get('/send', function(req, res) {
+//   twilio_client.sms.messages.post({
+//     to:'',
+//     from:'',
+//     body:'Hello World'
+//   }, function(error, message) {
+//       if (error) {
+//           console.log(error.message);
+//       }
+//   });
+// });
 
 server = app.listen(appEnv.port || 3000, appEnv.bind || 'localhost', function() {
   console.log('Up and running!');
