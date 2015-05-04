3b21dee32c0d2aa5dc6792e4cf09bab3d6e2ae2b
Sean Hellwig <seanhellwig@gmail.com>
2015-05-04 16:57:03 -0400

    Fix mongo connection

--- index.js
+++ index.js
@@ -11,30 +11,32 @@ var mongoUrl;
 var services = appEnv.services;
 var db;
    
-// if (services['mongodb-2.4']) {
-// 	mongoUrl = services['mongodb-2.4'].credentials.url;
-// }
+if (services['mongodb-2.4']) {
+	mongoUrl = services['mongodb-2.4'][0].credentials.url;
+}
 
-// mongo.connect(mongoUrl, function(err, db) {
-//   db = db;
-// });
+mongo.connect(mongoUrl, function(err, db) {
+  db = db;
+});
 
 app.post('/receive', function(req, res) {
 	var user = req.body.From;
 	var body = req.body.Body;
 });
 
-app.get('/send', function(req, res) {
-  // twilio_client.messages.create({
-  //   to:'',
-  //   from:'TWILIO_NUMBER',
-  //   body:'Hello World'
-  // }, function(error, message) {
-  //     if (error) {
-  //         console.log(error.message);
-  //     }
-  // });
-});
+
+// Test route
+// app.get('/send', function(req, res) {
+//   twilio_client.messages.create({
+//     to:'+13104093364',
+//     from:'TWILIO_NUMBER',
+//     body:'Hello World'
+//   }, function(error, message) {
+//       if (error) {
+//           console.log(error.message);
+//       }
+//   });
+// });
 
 server = app.listen(appEnv.port || 3000, appEnv.bind || 'localhost', function() {
   console.log('Up and running!');
