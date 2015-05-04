afdabc3b364fed606004dc91684b16eb4c799508
Sean Hellwig <seanhellwig@gmail.com>
2015-05-04 16:42:17 -0400

    Adds mock code for twilio + mongo

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
+// var Promise = require('bluebird');
 var express = require('express');
-var mongo = require('mongodb');
 var app = express();
-var twilio_client = require('twilio')(process.env.twilio_sid, process.env.twilio_token);
 var cfenv = require('cfenv');
 var appEnv = cfenv.getAppEnv();
+var mongo = require('mongodb').MongoClient;
+var twilio = require('twilio');
+// var twilio_client = twilio.RestClient(appEnv.twilio_sid, appEnv.twilio_token);
 var server;
+var mongoUrl;
+var services = appEnv.services;
+var db;
+   
+// if (services['mongodb-2.4']) {
+// 	mongoUrl = services['mongodb-2.4'].credentials.url;
+// }
 
-app.get('/receive', function(req, resp) {
-	console.log('/receive hit');
+// mongo.connect(mongoUrl, function(err, db) {
+//   db = db;
+// });
+
+app.post('/receive', function(req, res) {
+	var user = req.body.From;
+	var body = req.body.Body;
 });
 
+app.get('/send', function(req, res) {
+  // twilio_client.messages.create({
+  //   to:'',
+  //   from:'TWILIO_NUMBER',
+  //   body:'Hello World'
+  // }, function(error, message) {
+  //     if (error) {
+  //         console.log(error.message);
+  //     }
+  // });
+});
 
 server = app.listen(appEnv.port || 3000, appEnv.bind || 'localhost', function() {
-	console.log('Up and running!');
+  console.log('Up and running!');
 });
