fd2916e01c2b7f8c89706864ce5624947bdbcea0
Sean Hellwig <seanhellwig@gmail.com>
2015-05-04 20:23:52 -0400

    updates

--- index.js
+++ index.js
@@ -1,43 +1,65 @@
-// var Promise = require('bluebird');
+var Promise = require('bluebird');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var cfenv = require('cfenv');
+var prompt_engine = require('./prompt-engine');
 var appEnv = cfenv.getAppEnv();
-var mongo = require('mongodb').MongoClient;
+var mongo = Promise.promisifyAll(require('mongodb'));
 var twilio = require('twilio');
 var twilio_client = new twilio.RestClient(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+var twilio_post = Promise.promisify(twilio_client.sms.messages.post);
 var server;
-var mongoUrl;
+var mongoUrl = 'mongodb://localhost:27017/wsms';
 var services = appEnv.services;
-var db;
+var database;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
 
 if (services['mongodb-2.4']) {
 	mongoUrl = services['mongodb-2.4'][0].credentials.url;
 }
 
-mongo.connect(mongoUrl, function(err, db) {
-  db = db;
-});
+// mongo.connect(mongoUrl, function(err, db) {
+//   db = db;
+// });
 
-app.post('/receive', function(req, res) {
-	var user = req.body.From;
-	var body = req.body.Body;
-});
+mongo.connectAsync(mongoUrl).then(function(db){
+
+  app.post('/receive', function(req, res) {
+    console.log(req.body);
+
+  	var user = req.body.From;
+  	var body = req.body.Body;
+
+    return prompt_engine.askQuestion(db, user).then(function(question) {
+      console.log(question);
+      return twilio_post({
+          to: user,
+          from: '+18459432793',
+          body: question
+        }).then(function(message) {
+            console.log('Success sent');
+            console.log(message);
+        });
+    }).catch(function(e) {
+      console.log(e.message);
+    });
+
+  });
 
 
 // // Test route
 // app.get('/send', function(req, res) {
-//   twilio_client.sms.messages.post({
-//     to:'',
-//     from:'',
-//     body:'Hello World'
-//   }, function(error, message) {
-//       if (error) {
-//           console.log(error.message);
-//       }
-//   });
+//   
 // });
+  
 
-server = app.listen(appEnv.port || 3000, appEnv.bind || 'localhost', function() {
-  console.log('Up and running!');
+  console.log('Mongo connected');
+  database = db
+  server = app.listen(appEnv.port || 3000, appEnv.bind || 'localhost', function() {
+    console.log('Up and running!');
+  });
 });
